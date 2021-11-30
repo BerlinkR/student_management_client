91dd9d4456801b634c4d0689cba6cce83dc44bc9
berlink <2285525802@qq.com>
2021-11-30 17:16:44 +0800

    change information about input student

--- manage.go
+++ manage.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func input_student() (student){
-	fmt.Println("Please input student message: ")
+	fmt.Println("Please input student message:")
+	fmt.Println("Name  Id  Gender  MarkMath  MarkEnglish")
 	var nstu student
 	fmt.Scanln(&(nstu.Name),&(nstu.Id), &(nstu.Gender), &(nstu.MarkMath), &(nstu.MarkEnglish))
 	return nstu
